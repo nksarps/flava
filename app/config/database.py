230401a3d1a691aa583c0d2670534eae485805c1
kweku-xvi <nksarps@gmail.com>
2025-01-21 12:49:18 +0000

    updated database uri

--- app/config/database.py
+++ app/config/database.py
@@ -1,9 +1,10 @@
+from decouple import config
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_FILE_NAME = 'db.sqlite'
-SQLALCHEMY_DATABASE_URI = f'sqlite:///{SQLALCHEMY_FILE_NAME}'
+SQLALCHEMY_DATABASE_URI = config('DB_URI')
 
 engine = create_engine(SQLALCHEMY_DATABASE_URI, echo=True)
 SessionLocal = sessionmaker(engine, autoflush=False, autocommit=False)
