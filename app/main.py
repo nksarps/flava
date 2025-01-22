a65d7c598adb94c5946fdc7e83f16af9d37620d3
kweku-xvi <nksarps@gmail.com>
2025-01-22 11:22:11 +0000

    added users router

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
 from app.config.database import init_db
-from app.routers import recipe
+from app.routers import recipe, user
 
 from fastapi import FastAPI
 
 app = FastAPI()
 
 app.include_router(recipe.router)
+app.include_router(user.router)
