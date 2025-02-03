e8e693d68ac0d81f7b704fe73ada5f10d6089557
kweku-xvi <nksarps@gmail.com>
2025-02-03 11:01:18 +0000

    added redis cache for recipes

--- app/routers/recipe.py
+++ app/routers/recipe.py
@@ -1,3 +1,4 @@
+import redis, json
 from app.config.database import get_db
 from app.helpers.enums import MealType
 from app.models.recipe import Recipe
@@ -13,6 +14,8 @@ router = APIRouter(
     prefix="/recipes",
     tags=["recipes"]
 )
+
+rd = redis.Redis(host='localhost', port=6379, db=0)
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
 async def create_recipe(
@@ -40,8 +43,35 @@ async def get_recipes(
         session: Session = Depends(get_db),
         current_user: User = Depends(get_current_user)
 ) -> List[RecipeResponse]:
+
+    cached = rd.get('recipes')
+
+    if cached:
+        print('cache hit!')
+        return json.loads(cached)
+    
     recipes = session.query(Recipe).all()
-    return recipes
+
+    recipes_response = [
+        {
+            "id": str(recipe.id), 
+            "name": recipe.name,
+            "description": recipe.description,
+            "ingredients": recipe.ingredients,
+            "instructions": recipe.instructions,
+            "servings": recipe.servings,
+            "meal_type": recipe.meal_type,
+            "created_at": recipe.created_at.isoformat(),
+            "updated_at": recipe.updated_at.isoformat(),  
+        }
+        for recipe in recipes
+    ]
+
+
+    print('cache miss!')
+    rd.setex('recipes', 1800, json.dumps(recipes_response))
+
+    return recipes_response
 
 @router.get("/filter", status_code=status.HTTP_200_OK)
 async def filter_recipe_by_meal_type(
@@ -49,6 +79,13 @@ async def filter_recipe_by_meal_type(
     session: Session = Depends(get_db),
     current_user: User = Depends(get_current_user)
 ) -> List[RecipeResponse]:
+    cache_key = f'recipes:meal_type:{type.value}'
+
+    cached = rd.get(cache_key)
+    if cached:
+        print('cache hit!')
+        return json.loads(cached)
+
     recipes = session.query(Recipe).filter_by(meal_type=type).all()
 
     if not recipes:
@@ -57,7 +94,24 @@ async def filter_recipe_by_meal_type(
             detail=f'No recipes found with meal type {type.value}'
         )
 
-    return recipes
+    recipes_response = [
+        {
+            "id": str(recipe.id),
+            "name": recipe.name,
+            "description": recipe.description,
+            "ingredients": recipe.ingredients,
+            "instructions": recipe.instructions,
+            "servings": recipe.servings,
+            "meal_type": recipe.meal_type,
+            "created_at": recipe.created_at.isoformat(),
+            "updated_at": recipe.updated_at.isoformat()
+        } for recipe in recipes
+    ]
+
+    print('cache miss!')
+    rd.setex(cache_key, 3600, json.dumps(recipes_response))
+
+    return recipes_response
 
 @router.get("/search", status_code=status.HTTP_200_OK)
 async def search_recipe(
@@ -65,6 +119,13 @@ async def search_recipe(
     session: Session = Depends(get_db),
     current_user: User = Depends(get_current_user)
 ) -> List[RecipeResponse]:
+    cache_key = f"recipes:search:{name.lower()}"
+    cached = rd.get(cache_key)
+
+    if cached:
+        print("cache hit!")
+        return json.loads(cached)
+
     recipes = session.query(Recipe).filter(Recipe.name.ilike(f'%{name}%')).all()
 
     if not recipes:
@@ -73,7 +134,24 @@ async def search_recipe(
             detail=f'No recipes found with name containing, {name}'
         )
 
-    return recipes
+    recipes_response = [
+        {
+            "id": str(recipe.id),
+            "name": recipe.name,
+            "description": recipe.description,
+            "ingredients": recipe.ingredients,
+            "instructions": recipe.instructions,
+            "servings": recipe.servings,
+            "meal_type": recipe.meal_type,
+            "created_at": recipe.created_at.isoformat(),
+            "updated_at": recipe.updated_at.isoformat()
+        } for recipe in recipes
+    ]
+
+    print("cache miss!")
+    rd.setex(cache_key, 1800, json.dumps(recipes_response))
+
+    return recipes_response
 
 @router.get("/{id}", status_code=status.HTTP_200_OK)
 async def get_recipe_with_id(
@@ -81,6 +159,13 @@ async def get_recipe_with_id(
     session: Session = Depends(get_db),
     current_user: User = Depends(get_current_user)
 ) -> RecipeResponse:
+    cache_key = f"recipe:{id}"
+    cached = rd.get(cache_key)
+
+    if cached:
+        print("cache hit!")
+        return json.loads(cached)
+
     recipe = session.query(Recipe).get(id)
 
     if not recipe:
@@ -88,8 +173,23 @@ async def get_recipe_with_id(
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f'Recipe with ID, {id} does not exist'
         )
+
+    recipe_response = {
+        "id": str(recipe.id),
+        "name": recipe.name,
+        "description": recipe.description,
+        "ingredients": recipe.ingredients,
+        "instructions": recipe.instructions,
+        "servings": recipe.servings,
+        "meal_type": recipe.meal_type,
+        "created_at": recipe.created_at.isoformat(),
+        "updated_at": recipe.updated_at.isoformat(),
+    }
         
-    return recipe
+    print("cache miss!")
+    rd.setex(cache_key, 1800, json.dumps(recipe_response))
+
+    return recipe_response
 
 @router.put("/{id}", status_code=status.HTTP_200_OK)
 async def update_recipe(
@@ -120,6 +220,8 @@ async def delete_recipe(
     session: Session = Depends(get_db),
     current_user: User = Depends(get_current_user)
 ) -> None:
+    cache_key = f"recipe:{id}"
+
     recipe = session.query(Recipe).get(id)
 
     if not recipe:
@@ -130,3 +232,9 @@ async def delete_recipe(
 
     session.delete(recipe)
     session.commit()
+
+    rd.delete(cache_key)
+
+    print(f"Cache for recipe {id} has been invalidated.")
+
+    return None
